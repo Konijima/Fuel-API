0f281e54c0ee828107bfa37e86d6e9d7d93c5ad4
Konijima <mathieugermain1991@gmail.com>
2021-12-20 20:39:28 -0500

    Added Distribution file

---
+++ Contents/mods/FuelAPI/media/lua/server/Items/FuelAPI_Distribution.lua
@@ -0,0 +1,22 @@
+require "Items/SuburbsDistributions"
+require "Items/ProceduralDistributions"
+
+-- StoreShelfMechanics
+table.insert(ProceduralDistributions.list.StoreShelfMechanics.items, "FuelAPI.LargePetrolCan");
+table.insert(ProceduralDistributions.list.StoreShelfMechanics.items, 5);
+
+-- ArmySurplusMisc
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "FuelAPI.LargePetrolCanFull");
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 5);
+
+-- GarageMechanic
+table.insert(ProceduralDistributions.list.GarageMechanics.items, "FuelAPI.LargePetrolCan");
+table.insert(ProceduralDistributions.list.GarageMechanics.items, 5);
+
+-- CrateMechanics
+table.insert(ProceduralDistributions.list.CrateMechanics.items, "FuelAPI.LargePetrolCan");
+table.insert(ProceduralDistributions.list.CrateMechanics.items, 5);
+
+-- CarSupplyTools
+table.insert(ProceduralDistributions.list.CarSupplyTools.items, "FuelAPI.LargePetrolCan");
+table.insert(ProceduralDistributions.list.CarSupplyTools.items, 5);
